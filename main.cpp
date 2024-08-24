837745e8f0bf2249fbeb49b7f52975757ccb4549
michael <michael@peoplelikesoftware.com>
2024-08-23 23:43:56 -0400

    more things maybe

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 
 #define C_LENGTH 6
 
+/**
+ * CLIENT
+*/
+typedef struct 
+{
+	//for now client and auth/reso struct same between all
+	std::string email;
+	std::string password;
+	std::string id;
+
+}User;
+
+typedef struct
+{
+	std::string access_token;
+	std::string resource_name;
+	std::string expiry;
+}UserTokens;
+
+/**
+ * AUTH SERVER
+*/
 typedef struct 
 {
 	std::string account_name;
@@ -10,44 +32,85 @@ typedef struct
 	std::string auth_code;
 	std::string auth_secret;
 
+}OauthAccount;
 
-}User;
+typedef struct 
+{
+	std::string account_id;
+	std::string access_token;
+	std::string scope;
+	std::string expiry;
 
-void print_user(User user);
+}OauthUserTokens;
+
+/**
+ * RESOURCE SERVER
+*/
+
+
+
+
+void menu();
+void print_user(OauthAccount account);
 User create_user(std::string creds[]);
 
-int main()
+void print_account(OauthAccount account)
 {
-	std::string r[] = {"name", "123", "123456", "654321"};
-	User user = create_user(r);
-	print_user(user);
-	return 0;
+	std::cout << "accountname=" << account.account_name << std::endl
+		      << "account id=" << account.account_id << std::endl
+		      << "auth code=" << account.auth_code << std::endl
+		      << "auth secret=" << account.auth_secret << std::endl;
 }
 
-void print_user(User user)
+ OauthAccount create_account(std::string creds[])
 {
-	std::cout << "username=" << user.account_name << std::endl
-		      << "account id=" << user.account_id << std::endl
-		      << "auth code=" << user.auth_code << std::endl
-		      << "auth secret=" << user.auth_secret << std::endl;
-}
-
-User create_user(std::string creds[])
-{
-	User user = {
+	OauthAccount userAccount = {
 		creds[0],
 		creds[1],
 		creds[2],
 		creds[3]
 	};
 
-	return user;
+	return userAccount;
+}
+
+void display_menu()
+{
+	std::cout << "1)\n"
+				 "2)\n"
+				 "3)\n"
+				 "4)\n"
+				 "5)\n"
+				 "6)\n"
+				 "7)\n";
 }
 
 void menu()
 {
-	whille(true)
+	std::string input;
+	std::cout << "Please Select an option";
+	display_menu();
+	std::cin >> input;
+	do
 	{
-
-	}
+	}while(input != "7");
 }
+
+bool validate_user(const User& user)
+{
+	//check if user exits in users file
+	return true;
+}
+
+
+
+
+
+
+int main()
+{
+	std::string r[] = {"name", "123", "123456", "654321"};
+	menu();
+	return 0;
+}
+
